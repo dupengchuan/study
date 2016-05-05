23604e6c0fbfee3f37b775338d96823f2ddf8b61
root <root@localhost.localdomain>
2016-05-05 17:38:14 +0800

    add

---
+++ src/main/java/dpc/leetcode/Solution11_0.java
@@ -0,0 +1,27 @@
+package dpc.leetcode;
+
+public class Solution11_0 {
+	public int maxArea(int[] height) {
+		int l = 0;
+		int r = height.length - 1;
+		int maxWater = -1;
+		int water = -1;
+		while(l < r){
+			water = Math.min(height[l], height[r]) * (r - l);
+			if(water > maxWater){
+				maxWater = water;
+			}
+			if(height[l] < height[r]){
+				l++;
+			}else{
+				r--;
+			}
+		}
+		return maxWater;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/java/dpc/leetcode/Solution12.java
@@ -0,0 +1,53 @@
+package dpc.leetcode;
+
+public class Solution12 {
+	
+	public String intToRoman(int num) {
+        
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 4 && num != 0; i++){
+			sb.insert(0,this.getStrBuff(num % 10,i));
+			num /= 10;
+		}
+		return sb.toString();
+    }
+	
+	private char[] pre = new char[]{'I','X','C','M'};
+	private char[] later = new char[]{'V','L','D'};
+	
+	private StringBuffer getStrBuff(int n, int weight){
+		StringBuffer sb = new StringBuffer();
+		if(n == 1 || n == 2 || n == 3){
+			for(int i = 0; i < n; i++){
+				sb.append(pre[weight]);
+			}
+			return sb;
+		}
+		if(n == 4){
+			sb.append(pre[weight]);
+			sb.append(later[weight]);
+			return sb;
+		}
+		if(n == 5 || n == 6 || n == 7 || n == 8){
+			sb.append(later[weight]);
+			for(int i = 5; i < n; i++){
+				sb.append(pre[weight]);
+			}
+			return sb;
+		}
+		if(n == 9){
+			sb.append(pre[weight]);
+			sb.append(pre[weight+1]);
+			return sb;
+		}
+		return sb;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution12 solution = new Solution12();
+		System.out.println(solution.intToRoman(3999));
+		System.out.println(solution.intToRoman(1));
+	}
+
+}
